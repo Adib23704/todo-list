2daaea6425f91f6f407df4ddb45e4b4c095fccf3
Adib23704 <adib23704@gmail.com>
2023-09-23 01:30:15 +0600

    Added local storage saving system

--- public/script.js
+++ public/script.js
@@ -10,70 +10,75 @@ document.addEventListener('DOMContentLoaded', function () {
         tasks.forEach(function (task, index) {
             const listItem = document.createElement('li');
             listItem.innerHTML = `
-                <span class="task-text" data-index="${index}">${task}</span>
-                <button class="edit-button">Edit</button>
-                <button class="delete-button" data-index="${index}">Delete</button>
+                <div class="task-item">
+                    <span class="task-text" data-index="${index}">${task}</span>
+                    <div class="button-container">
+                        <button class="edit-button" data-index="${index}">Edit</button>
+                        <button class="delete-button" data-index="${index}">Delete</button>
+                    </div>
+                </div>
             `;
             taskList.appendChild(listItem);
         });
         addEditListeners();
     }
 
-    // Function to add event listeners for editing tasks
+    // Function to save tasks to local storage
+    function saveTasksToLocalStorage() {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    // Function to retrieve tasks from local storage
+    function getTasksFromLocalStorage() {
+        const storedTasks = localStorage.getItem('tasks');
+        if (storedTasks) {
+            tasks = JSON.parse(storedTasks);
+            renderTasks();
+        }
+    }
+
+    // Event listener for form submission
+    taskForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const taskText = taskInput.value.trim();
+        if (taskText !== '') {
+            tasks.push(taskText);
+            taskInput.value = '';
+            saveTasksToLocalStorage();
+            renderTasks();
+        }
+    });
+
+    // Function to add event listeners to Edit and Delete buttons
     function addEditListeners() {
-        const taskTexts = document.querySelectorAll('.task-text');
         const editButtons = document.querySelectorAll('.edit-button');
+        const deleteButtons = document.querySelectorAll('.delete-button');
 
-        editButtons.forEach(function (button, index) {
+        editButtons.forEach(function (button) {
             button.addEventListener('click', function () {
-                const taskText = taskTexts[index];
-                const currentTask = taskText.innerText;
-                const editInput = document.createElement('input');
-                editInput.type = 'text';
-                editInput.value = currentTask;
-                taskText.innerHTML = '';
-                taskText.appendChild(editInput);
-                editInput.focus();
-
-                // Save changes when Enter is pressed or input loses focus
-                editInput.addEventListener('blur', function () {
-                    const newTaskText = editInput.value.trim();
-                    tasks[index] = newTaskText;
+                const index = button.getAttribute('data-index');
+                const newText = prompt('Edit the task:', tasks[index]);
+                if (newText !== null) {
+                    tasks[index] = newText;
+                    saveTasksToLocalStorage();
                     renderTasks();
-                });
+                }
+            });
+        });
 
-                editInput.addEventListener('keyup', function (event) {
-                    if (event.key === 'Enter') {
-                        const newTaskText = editInput.value.trim();
-                        tasks[index] = newTaskText;
-                        renderTasks();
-                    }
-                });
+        deleteButtons.forEach(function (button) {
+            button.addEventListener('click', function () {
+                const index = button.getAttribute('data-index');
+                if (confirm('Are you sure you want to delete this task?')) {
+                    tasks.splice(index, 1);
+                    saveTasksToLocalStorage();
+                    renderTasks();
+                }
             });
         });
     }
 
-    // Event listener for adding tasks
-    taskForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const newTask = taskInput.value.trim();
-        if (newTask !== '') {
-            tasks.push(newTask);
-            taskInput.value = '';
-            renderTasks();
-        }
-    });
-
-    // Event listener for deleting tasks
-    taskList.addEventListener('click', function (e) {
-        if (e.target.classList.contains('delete-button')) {
-            const index = e.target.getAttribute('data-index');
-            tasks.splice(index, 1);
-            renderTasks();
-        }
-    });
-
     // Initial rendering
-    renderTasks();
-
+    getTasksFromLocalStorage();
+    // renderTasks();
 });
